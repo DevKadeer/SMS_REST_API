ece9ca1e0e568249c1fbc8b13ba38791b1db4c98
Kadeer Khan <kadeer.khan@aciworldwide.com>
2020-11-26 17:11:08 +0530

    Added swagger

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -12,6 +12,7 @@ using SMS_REST_API.Repositories;
 
 namespace SMS_REST_API.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class CityController : ControllerBase
@@ -25,6 +26,24 @@ namespace SMS_REST_API.Controllers
             _cityRepository = cityRepository;
         }
 
+        /// <summary>
+        /// Creates a TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="item"></param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>     
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CityModel>>> GetAllCities()
         {
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +34,8 @@ namespace SMS_REST_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+
             services.AddControllers();
 
             services.AddCors();
@@ -37,13 +43,42 @@ namespace SMS_REST_API
             services.AddDbContext<CityContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<ICityRepository, CityRepository>();
+
+            services.AddSwaggerGen(); 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "SMS Group API",
+                    Description = "SMS Group assignment",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Kadeer Khan",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SMS API");
+            });
 
-            // Make sure you call this before calling app.UseMvc()
+
             app.UseCors(
                 options => options.WithOrigins("*").AllowAnyMethod()
             );
